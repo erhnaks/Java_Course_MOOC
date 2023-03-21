38fd6e0916ab889ca0de6c2a0c0eb4654e32d93e
erhnaks <erhnaks@hotmail.com>
2023-03-21 12:41:32 +0000

    String Concatenation in Java

---
+++ src/com/erhan/concatenation/HiConcatenation.java
@@ -0,0 +1,10 @@
+package com.erhan.concatenation;
+
+public class HiConcatenation {
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        System.out.println("Hi " + name);
+
+
+    }
+}
