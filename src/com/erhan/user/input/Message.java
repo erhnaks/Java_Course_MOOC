2afb08c70db47135dda156160ab6bde25afa1543
erhnaks <erhnaks@hotmail.com>
2023-03-21 12:32:33 +0000

    Scanner util

---
+++ src/com/erhan/user/input/Message.java
@@ -0,0 +1,13 @@
+package com.erhan.user.input;
+import java.util.Scanner;
+
+public class Message {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your message? ");
+        String message = scanner.nextLine();
+
+        System.out.println(message);
+
+    }
+}
