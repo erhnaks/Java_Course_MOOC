073dc7a938527c4cfe7422038b8f70fb54506e49
erhnaks <erhnaks@hotmail.com>
2023-03-21 15:34:05 +0000

    Sandpit package has been added to play around

---
+++ src/com/erhan/sand_pit/ConvertingValues.java
@@ -0,0 +1,11 @@
+package com.erhan.sand_pit;
+
+public class ConvertingValues {
+    public static void main(String[] args) {
+        double num = 0.4134234;
+        System.out.println(num);
+        System.out.println("After it is re-assigned-------");
+        num=1;
+        System.out.println(num);
+    }
+}
