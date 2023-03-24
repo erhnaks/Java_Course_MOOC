0107abad678622dc7ce58bd07ca8e6f1b74680e5
erhnaks <erhnaks@hotmail.com>
2023-03-24 11:37:48 +0000

    Squared Root of Sum exercise

---
+++ src/com/erhan/part_two/problems_patterns/SquareRootOfSum.java
@@ -0,0 +1,14 @@
+package com.erhan.part_two.problems_patterns;
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+
+        System.out.println(Math.sqrt(numOne + numTwo));
+    }
+}
