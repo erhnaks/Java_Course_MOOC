3b3c9300680533e185dae3f1595c9081d450f9ba
erhnaks <erhnaks@hotmail.com>
2023-03-25 16:10:59 +0000

    Sum of numbers

---
+++ src/com/erhan/part_two/repeating_functionality/SumOfNumbers.java
@@ -0,0 +1,24 @@
+package com.erhan.part_two.repeating_functionality;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int num = scanner.nextInt();
+
+            if (num == 0) {
+                break;
+            }
+            if (num !=0 ) {
+                sum = sum + num;
+            }
+        }
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
