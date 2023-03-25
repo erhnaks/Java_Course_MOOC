665412b80e8e4ef1b3b678baffdcc51b8e7c9aa1
erhnaks <erhnaks@hotmail.com>
2023-03-25 11:47:59 +0000

    while true exercise

---
+++ src/com/erhan/sandpit/WhileTrueExercise.java
@@ -0,0 +1,25 @@
+package com.erhan.sandpit;
+
+import java.util.Scanner;
+
+public class WhileTrueExercise {
+    public static void main (String [] args){
+        int num = 1;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = scan.nextInt();
+        System.out.println("------------------------------------------");
+
+        while(true){
+            System.out.println(num);
+          if (num >= input){
+              break;
+          }
+            num = num + 1;
+        }
+
+        System.out.println("------------------------------------------");
+        System.out.println("Ready!");
+    }
+}
