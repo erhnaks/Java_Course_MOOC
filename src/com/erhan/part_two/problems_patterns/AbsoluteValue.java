a388c9506844f5db18338e4b02aef5a24dca1042
erhnaks <erhnaks@hotmail.com>
2023-03-25 10:35:23 +0000

    Absolute value exercise

---
+++ src/com/erhan/part_two/problems_patterns/AbsoluteValue.java
@@ -0,0 +1,17 @@
+package com.erhan.part_two.problems_patterns;
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println(num * -1);
+        } else {
+            System.out.println(num);
+        }
+    }
+}
