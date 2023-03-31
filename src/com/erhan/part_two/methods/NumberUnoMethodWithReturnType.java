f1f9e18f18d57240562346501ce661c3dab12753
erhnaks <erhnaks@hotmail.com>
2023-03-31 18:59:19 +0100

    Methods in Java with return type

---
+++ src/com/erhan/part_two/methods/NumberUnoMethodWithReturnType.java
@@ -0,0 +1,22 @@
+package com.erhan.part_two.methods;
+
+public class NumberUnoMethodWithReturnType {
+    public static int numberUno() {
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+        int number= numberUno();
+        System.out.println(number);
+        System.out.println(numberUno());
+
+        int num1 = numberUno() + 5;
+
+        System.out.println(num1);
+
+
+    }
+
+}
