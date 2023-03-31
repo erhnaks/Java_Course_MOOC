b9ffd293fa5c9fae6f9c8295b6e1d4f31226672c
erhnaks <erhnaks@hotmail.com>
2023-03-31 14:47:56 +0100

    methods in Java

---
+++ src/com/erhan/part_two/methods/MethodCalling.java
@@ -0,0 +1,19 @@
+package com.erhan.part_two.methods;
+
+import java.util.Scanner;
+
+public class MethodCalling {
+    public static void main (String[ ] args) {
+        String name= "";
+        greet(name);
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hi, My name is Java. What is your name?");
+        Scanner scan = new Scanner(System.in);
+        name = scan.nextLine();
+        System.out.println("Hello, " + name + "! Nice to meet you!");
+    }
+
+
+}
