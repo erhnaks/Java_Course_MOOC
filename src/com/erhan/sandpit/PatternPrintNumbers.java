b13744e9742262715faef31759800bc1ace28eb8
erhnaks <erhnaks@hotmail.com>
2023-04-02 13:42:30 +0100

    Indentention has been corrected

--- src/com/erhan/sandpit/PatternPrintNumbers.java
+++ src/com/erhan/sandpit/PatternPrintNumbers.java
@@ -6,12 +6,12 @@ public class PatternPrintNumbers {
     public static void main(String[] args) {
         int num = numberOfTimes(5);
 
-      for (int i = 1; i <= num; i++) {
-          for (int j = 1; j <= num; j++) {
-              System.out.print(j);
-          }
-          System.out.println();
-      }
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static int numberOfTimes(int num) {
