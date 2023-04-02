1a9fd2be1eb5f1e436e67a345f3647c75bb1c4e2
erhnaks <erhnaks@hotmail.com>
2023-04-02 16:15:19 +0100

    Triangle printing exercise has been completed

--- src/com/erhan/sandpit/TrianglePatternPrinting.java
+++ src/com/erhan/sandpit/TrianglePatternPrinting.java
@@ -1,7 +1,24 @@
 package com.erhan.sandpit;
 
+import java.util.Scanner;
+
 public class TrianglePatternPrinting {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to start drawing your Triangle: ");
+        int triangle = sc.nextInt();
+
+        for (int i = 1; i <= triangle; i++) {
+            for (int s = 1; s <= triangle - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 1; star <= 2 * i - 1; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
