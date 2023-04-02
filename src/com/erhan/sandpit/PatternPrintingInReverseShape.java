86934a49663367b7dc3966fb3939e579a8e30b25
erhnaks <erhnaks@hotmail.com>
2023-04-02 16:00:59 +0100

    printing patterns exercise

--- src/com/erhan/sandpit/PatternPrintingInReverseShape.java
+++ src/com/erhan/sandpit/PatternPrintingInReverseShape.java
@@ -2,5 +2,19 @@ package com.erhan.sandpit;
 
 public class PatternPrintingInReverseShape {
 
+    public static void main(String[] args) {
 
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+
+            for (int s = 1; s <= num - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 }
