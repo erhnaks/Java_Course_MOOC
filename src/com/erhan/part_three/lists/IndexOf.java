95ef91f232b280d0270cc0162262e3c8daa792ae
erhnaks <erhnaks@hotmail.com>
2023-04-03 18:23:31 +0100

    index of exercise

---
+++ src/com/erhan/part_three/lists/IndexOf.java
@@ -0,0 +1,33 @@
+package com.erhan.part_three.lists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // implement here finding the indices of a number
+        System.out.print("Search for? ");
+        int searchForNumber = scanner.nextInt();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (searchForNumber == list.get(i)) {
+                System.out.println(searchForNumber + " is at index " + i);
+            }
+        }
+    }
+}
