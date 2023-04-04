ffdec2dd16fb9d329359bcbb6dab4fc10112e260
erhnaks <erhnaks@hotmail.com>
2023-04-04 20:20:01 +0100

    String comparison

---
+++ src/com/erhan/part_three/strings/LoginUsingStringComparison.java
@@ -0,0 +1,23 @@
+package com.erhan.part_three.strings;
+
+import java.util.Scanner;
+
+public class LoginUsingStringComparison {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        if (username.equals("alex") && password.equals("sunshine") ||
+                username.equals("emma") && password.equals("haskell")) {
+            System.out.println("You have successfully logged in!");
+        } else
+            System.out.println("Incorrect username or password!");
+
+
+    }
+}
