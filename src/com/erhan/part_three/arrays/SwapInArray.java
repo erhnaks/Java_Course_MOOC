b740292a9f1e642938b00ef5254dd619abcab604
erhnaks <erhnaks@hotmail.com>
2023-04-04 18:33:14 +0100

    Arrays in Java

---
+++ src/com/erhan/part_three/arrays/SwapInArray.java
@@ -0,0 +1,46 @@
+package com.erhan.part_three.arrays;
+
+import java.util.Scanner;
+
+public class SwapInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        // Implement here
+        // asking for the two indices
+        // and then swapping them
+
+        System.out.println("Give two indices to swap: ");
+        int firstIndex = scanner.nextInt();
+        int secondIndex = scanner.nextInt();
+
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
+/*The exercise template already contains a program, that creates an array and prints the values of the array twice.
+Modify the program to do following: After the first printing, the program should ask for two indices from the user.
+The values in these two indices should be swapped, and in the end the values of the array should be printed once again.*/
