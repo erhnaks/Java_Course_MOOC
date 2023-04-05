990bc61d500e4498ec9ad56f567f189842731a45
erhnaks <erhnaks@hotmail.com>
2023-04-05 10:31:40 +0100

    Person class created

---
+++ src/com/erhan/part_four/oop/person/Main.java
@@ -0,0 +1,7 @@
+package com.erhan.part_four.oop.person;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/erhan/part_four/oop/person/Person.java
@@ -0,0 +1,6 @@
+package com.erhan.part_four.oop.person;
+
+public class Person {
+    private String name;
+    private int age;
+}
