555099873bc6b1f04e7f9eb13cd16e5a044761d1
erhnaks <erhnaks@hotmail.com>
2023-04-05 11:32:07 +0100

    Creating an object with only a method in it

---
+++ src/com/erhan/part_four/oop/door/Door.java
@@ -0,0 +1,7 @@
+package com.erhan.part_four.oop.door;
+
+public class Door {
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
---
+++ src/com/erhan/part_four/oop/door/Main.java
@@ -0,0 +1,10 @@
+package com.erhan.part_four.oop.door;
+
+public class Main {
+    public static void main(String[] args) {
+        Door alexander = new Door();
+
+        alexander.knock();
+        alexander.knock();
+    }
+}
