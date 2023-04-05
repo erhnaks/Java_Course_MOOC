fa65d2f7a5066899ab468e598ed6068ddcc5eb2b
erhnaks <erhnaks@hotmail.com>
2023-04-05 09:59:45 +0100

    oop exercise in java

---
+++ src/com/erhan/part_four/oop/your_first_bank_transfer/YourFirstBankTransfer.java
@@ -0,0 +1,20 @@
+package com.erhan.part_four.oop.your_first_bank_transfer;
+
+public class YourFirstBankTransfer {
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // write your program here
+
+        Account matthewsAccount = new Account("Matthews account", 1000);
+        Account myAccount = new Account("My account", 0.0);
+
+        matthewsAccount.withdrawal(100.00);
+
+        myAccount.deposit(100.00);
+
+        System.out.println(matthewsAccount);
+        System.out.println(myAccount);
+
+
+    }
+}
