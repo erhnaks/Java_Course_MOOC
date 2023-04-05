e24760d1b89a855e3c1c2ae97c0d8281cd6631be
erhnaks <erhnaks@hotmail.com>
2023-04-05 10:37:28 +0100

    Constructor in classÂ

--- src/com/erhan/part_four/oop/person/Main.java
+++ src/com/erhan/part_four/oop/person/Main.java
@@ -3,5 +3,9 @@ package com.erhan.part_four.oop.person;
 public class Main {
     public static void main(String[] args) {
 
+        Person eric = new Person("Eric");
+
+//        System.out.println(eric);
+
     }
 }
--- src/com/erhan/part_four/oop/person/Person.java
+++ src/com/erhan/part_four/oop/person/Person.java
@@ -3,4 +3,10 @@ package com.erhan.part_four.oop.person;
 public class Person {
     private String name;
     private int age;
+
+    public Person (String initialName) {
+        this.age = 0;
+        this.name = initialName;
+
+    }
 }
