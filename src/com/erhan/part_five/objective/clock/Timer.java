aab88b8412f05a0bcac41e6cc8192e0ca732f9e1
erhnaks <erhnaks@hotmail.com>
2023-04-08 15:10:43 +0100

    Timer method created

--- src/com/erhan/part_five/objective/clock/Timer.java
+++ src/com/erhan/part_five/objective/clock/Timer.java
@@ -1,4 +1,23 @@
 package com.erhan.part_five.objective.clock;
 
 public class Timer {
+    private ClockHand hundreths;
+    private ClockHand seconds;
+
+    public Timer() {
+        this.hundreths = new ClockHand(100);
+        this.seconds = new ClockHand(60);
+    }
+
+    public String toString() {
+        return this.seconds + ":" + this.hundreths;
+    }
+
+    public void advance() {
+        this.hundreths.advance();
+
+        if (this.hundreths.value() == 0) {
+            this.seconds.advance();
+        }
+    }
 }
