2b5b9d11d40f46a098ccc6f398e94fbd56391c94
erhnaks <erhnaks@hotmail.com>
2023-04-08 16:08:37 +0100

    Cube calculator

--- src/com/erhan/part_five/objective/cube/Cube.java
+++ src/com/erhan/part_five/objective/cube/Cube.java
@@ -2,4 +2,22 @@ package com.erhan.part_five.objective.cube;
 
 public class Cube {
     private int edgeLength;
+
+    public Cube(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+
+    public int volume() {
+        return edgeLength * edgeLength * edgeLength;
+    }
+
+    public int getEdgeLength() {
+        return this.edgeLength;
+    }
+
+    public String toString() {
+        return "The edge is " + this.edgeLength + ", and the volume is: " + volume();
+    }
+
+
 }
