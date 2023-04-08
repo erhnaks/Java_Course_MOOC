d813d7cad1314a0f375968b83f28f85643b94545
erhnaks <erhnaks@hotmail.com>
2023-04-08 15:11:21 +0100

    Clock components broken into smaller parts

--- src/com/erhan/part_five/objective/clock/ClockHand.java
+++ src/com/erhan/part_five/objective/clock/ClockHand.java
@@ -1,4 +1,31 @@
 package com.erhan.part_five.objective.clock;
 
 public class ClockHand {
+    private int value;
+    private int limit;
+
+    public ClockHand(int limit) {
+        this.limit = limit;
+        this.value = 0;
+    }
+
+    public void advance() {
+        this.value = this.value + 1;
+
+        if (this.value >= this.limit) {
+            this.value = 0;
+        }
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public String toString() {
+        if (this.value < 10) {
+            return "0" + this.value;
+        }
+
+        return "" + this.value;
+    }
 }
