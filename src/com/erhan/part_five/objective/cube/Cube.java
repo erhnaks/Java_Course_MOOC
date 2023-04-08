d0092c933aa7a95637c396ce829a7f7c4d090dd4
erhnaks <erhnaks@hotmail.com>
2023-04-08 16:06:00 +0100

    Cube lentgh calculator main

---
+++ src/com/erhan/part_five/objective/cube/Cube.java
@@ -0,0 +1,5 @@
+package com.erhan.part_five.objective.cube;
+
+public class Cube {
+    private int edgeLength;
+}
---
+++ src/com/erhan/part_five/objective/cube/Main.java
@@ -0,0 +1,9 @@
+package com.erhan.part_five.objective.cube;
+
+public class Main {
+    public static void main(String[] args) {
+        Cube salt = new Cube(2);
+        System.out.println(salt.volume());
+        System.out.println(salt);
+    }
+}
