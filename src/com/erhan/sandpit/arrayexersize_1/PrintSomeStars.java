4ae0daaf2cb2436d57ee338db9c4eb763410e0f0
erhnaks <erhnaks@hotmail.com>
2023-04-09 12:46:43 +0100

    done some exercise

---
+++ src/com/erhan/sandpit/arrayexersize_1/PrintSomeStars.java
@@ -0,0 +1,41 @@
+package com.erhan.sandpit.arrayexersize_1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintSomeStars {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+
+        System.out.println("Enter numbers: ");
+
+        while (true) {
+
+            num = sc.nextInt();
+
+            if (num == -1) {
+                break;
+            }
+
+            numbers.add(num);
+        }
+        System.out.println(numbers);
+
+        for (int i = 0; i < numbers.size(); i ++) {
+
+            for (int j = 1; j <= numbers.get(i); j++  ) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+
+}
