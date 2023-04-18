d4575be0f3f819629acea8e123105d7af5c1e6da
erhnaks <erhnaks@hotmail.com>
2023-04-18 09:02:44 +0100

    Compairing apartment exercise

---
+++ src/com/erhan/part_five/primitive_and_reference_variables/compairing_apartments/MainProgram.java
@@ -0,0 +1,14 @@
+package com.erhan.part_five.primitive_and_reference_variables.compairing_apartments;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        // you can write code here to try out your program
+        Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+        Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+        Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+
+        System.out.println(manhattanStudioApt.priceDifference(atlantaTwoBedroomApt));  //71600
+        System.out.println(bangorThreeBedroomApt.priceDifference(atlantaTwoBedroomApt));   //35400
+
+    }
+}
