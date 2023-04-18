0ae102406bd9e81431fd8d1ac4e299d2e26d7ad3
erhnaks <erhnaks@hotmail.com>
2023-04-18 09:02:03 +0100

    Compairing apartments exercise in Java

---
+++ src/com/erhan/part_five/primitive_and_reference_variables/compairing_apartments/Apartment.java
@@ -0,0 +1,26 @@
+package com.erhan.part_five.primitive_and_reference_variables.compairing_apartments;
+
+public class Apartment {
+
+    private int rooms;
+    private int squares;
+    private int pricePerSquare;
+
+    public Apartment(int rooms, int squares, int pricePerSquare) {
+        this.rooms = rooms;
+        this.squares = squares;
+        this.pricePerSquare = pricePerSquare;
+    }
+    public boolean largerThan(Apartment compared) {
+        return this.squares > compared.squares;
+    }
+
+    public int priceDifference(Apartment compared) {
+        return Math.abs((compared.squares * compared.pricePerSquare) - (this.squares * this.pricePerSquare));
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+        return (this.squares * this.pricePerSquare) > (compared.squares * compared.pricePerSquare);
+    }
+
+}
