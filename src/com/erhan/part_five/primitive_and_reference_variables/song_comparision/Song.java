fac02233aedb8a499eb96fc4c60816c816feeea7
erhnaks <erhnaks@hotmail.com>
2023-04-18 08:59:35 +0100

    Song comparision

---
+++ src/com/erhan/part_five/primitive_and_reference_variables/song_comparision/Song.java
@@ -0,0 +1,40 @@
+package com.erhan.part_five.primitive_and_reference_variables.song_comparision;
+
+public class Song {
+
+    private String artist;
+    private String name;
+    private int durationInSeconds;
+
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+
+    public boolean equals(Object compared) {
+
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+
+        Song comparedSong = (Song) compared;
+
+        if (this.artist.equals(comparedSong.artist) &&
+                this.name.equals(comparedSong.name) &&
+                this.durationInSeconds == comparedSong.durationInSeconds
+        ) {
+            return true;
+        }
+        return false;
+    }
+}
