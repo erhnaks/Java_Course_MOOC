6efdf247279627a6bc1a16b4cbdb474b139fc76e
erhnaks <erhnaks@hotmail.com>
2023-04-20 12:07:05 +0100

    is it on the list exercise breaking down the programming

---
+++ src/com/erhan/sandpit/breaking_down_programming/is_it_on_the_list/MainProgram.java
@@ -0,0 +1,7 @@
+package com.erhan.sandpit.breaking_down_programming.is_it_on_the_list;
+
+public class MainProgram {
+    public static void main (String[] args) {
+//        System.out.println("Hello");
+    }
+}
