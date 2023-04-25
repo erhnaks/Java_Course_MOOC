adc3a5229d306b34d3ceaae4b6a4fd889429658a
erhnaks <erhnaks@hotmail.com>
2023-04-25 10:36:12 +0100

    Building a simple calculator for testing purposes. Intro to testing

--- src/com/erhan/sandpit/breaking_down_programming/is_it_on_the_list/MainProgram.java
+++
@@ -1,7 +0,0 @@
-package com.erhan.sandpit.breaking_down_programming.is_it_on_the_list;
-
-public class MainProgram {
-    public static void main (String[] args) {
-//        System.out.println("Hello");
-    }
-}
---
+++ src/com/erhan/sandpit/java_testing/calculator_junit_testing/MainProgram.java
@@ -0,0 +1,7 @@
+package com.erhan.sandpit.java_testing.calculator_junit_testing;
+
+public class MainProgram {
+    public static void main (String[] args) {
+        System.out.println("Hello");
+    }
+}
