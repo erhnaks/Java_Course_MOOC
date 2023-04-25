04c1e8327df6faf54915dea33f65a56a9eb413fc
erhnaks <erhnaks@hotmail.com>
2023-04-25 10:41:08 +0100

    ÂCalculator class has been created

---
+++ src/com/erhan/sandpit/java_testing/calculator_junit_testing/Calculator.java
@@ -0,0 +1,22 @@
+package com.erhan.sandpit.java_testing.calculator_junit_testing;
+
+public class Calculator {
+
+    private int value;
+
+    public Calculator() {
+        this.value = 0;
+    }
+
+    public void add(int num) {
+        this.value += num;
+    }
+
+    public void subtract(int num) {
+        this.value += num;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
--- src/com/erhan/sandpit/java_testing/calculator_junit_testing/MainProgram.java
+++ src/com/erhan/sandpit/java_testing/calculator_junit_testing/MainProgram.java
@@ -2,6 +2,6 @@ package com.erhan.sandpit.java_testing.calculator_junit_testing;
 
 public class MainProgram {
     public static void main (String[] args) {
-        System.out.println("Hello");
+
     }
 }
